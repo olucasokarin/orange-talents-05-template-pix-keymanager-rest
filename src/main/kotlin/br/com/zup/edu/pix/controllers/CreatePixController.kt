46340c61d0ec0e4a309a97ff399ce33fc5b62f81
olucasokarin <olucas014@hotmail.com>
2021-07-19 14:46:55 -0300

    register a new key works

--- src/main/kotlin/br/com/zup/edu/pix/controllers/CreatePixController.kt
+++ src/main/kotlin/br/com/zup/edu/pix/controllers/CreatePixController.kt
@@ -1,13 +1,19 @@
 package br.com.zup.edu.pix.controllers
 
+import br.com.zupedu.grpc.PixServiceGrpc
+import br.com.zupedu.grpc.RegisterPixRequest
 import br.com.zup.edu.pix.enums.TypeAccount
 import br.com.zup.edu.pix.enums.TypeKey
 import br.com.zup.edu.pix.validations.ValidPixKey
 import io.micronaut.core.annotation.Introspected
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.MutableHttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.uri.UriBuilder
 import io.micronaut.validation.Validated
+import javax.inject.Inject
 import javax.validation.Valid
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
@@ -15,13 +21,22 @@ import javax.validation.constraints.Size
 
 @Controller("api/pix")
 @Validated
-class CreatePixController {
+class CreatePixController(
+    @Inject private val registerPixClient: PixServiceGrpc.PixServiceBlockingStub
+) {
 
     @Post
-    fun create(@Valid @Body pixKey: PixKeyRequest) {
+    fun create(@Valid @Body pixKey: PixKeyRequest): MutableHttpResponse<Any>? {
 
-        println(pixKey)
+        val registerResponse = registerPixClient.register(pixKey.requestRestToRequestGrpc())
 
+        val uri = UriBuilder.of("api/pix/{idPix}")
+            .expand(mutableMapOf(Pair("idPix", registerResponse.idPix)))
+
+        return HttpResponse.created<Any?>(uri)
+            .body(
+                mutableMapOf(Pair("idClient", registerResponse.idClient),
+                Pair("idPix", registerResponse.idPix)))
     }
 }
 
@@ -36,4 +51,12 @@ data class PixKeyRequest(
     val valueKey: String?,
     @field:NotNull
     val typeAccount: TypeAccount,
-)
+) {
+    fun requestRestToRequestGrpc() =
+        RegisterPixRequest.newBuilder()
+            .setIdClient(idClient)
+            .setTypeKey(br.com.zupedu.grpc.TypeKey.valueOf(typeKey.name))
+            .setValueKey(valueKey)
+            .setTypeAccount(br.com.zupedu.grpc.TypeAccount.valueOf(typeAccount.name))
+            .build()
+}
